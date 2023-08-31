470bb79e58fbe358c99bf7020c9f56ac7cd8d92c
rishabhkumar <1741rishabh@gmail.com>
2023-08-31 14:32:59 +0530

    no errors

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,8 @@
-"use client"
-import { useTheme } from 'next-themes'
-import { useEffect } from 'react'
+
+
 export default function profile()
 {
-    const { theme, setTheme } = useTheme()
-   useEffect(()=>{
-    console.log(theme);
     
-   },[theme])
    
     
     return <><h1>Profile  </h1></>
